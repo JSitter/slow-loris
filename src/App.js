1fb30827713ac12919b556b3e45d1c7a4195e92b
Justin Sitter <justin@jaytria.com>
2018-03-02 13:09:03 -0800

    change some things

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="Slow Loris">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Slow Loris</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Slow loris doesn't swim and certainly doesn't like to get wet. 
         </p>
       </div>
     );
